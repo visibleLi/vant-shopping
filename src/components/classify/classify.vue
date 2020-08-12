e9bfd3c94e7809d70bd998e1501ceacac36308f2
lmany <864494871@qq.com>
2020-08-12 09:33:12 +0800

    分类

--- src/components/classify/classify.vue
+++ src/components/classify/classify.vue
@@ -1,66 +1,100 @@
 <template>
-    <div>
-
-
-        <van-tree-select height="100%" :items="items" :main-active-index.sync="active" @click-nav="changeData">
-            <template #content>
-                <div v-for="(l1,index1) in items" :key="index1">
-                    <div v-for="(l2,index2) in l1.children" :key="index2">
-                        <van-grid :border="true" :column-num="3" square clickable>
-                            <van-grid-item v-for="(l3,index3) in l2.children" :key="index3">
-                                <van-image v-if="active === 0" :src="l3.cat_icon" show-loading/>
-                            </van-grid-item>
-                        </van-grid>
-                    </div>
-                </div>
-
-            </template>
-        </van-tree-select>
-    </div>
+  <div>
+    <van-tree-select height="100%" :items="items" :main-active-index.sync="active" @click-nav="changeData">
+      <template #content>
+        <div v-for="(l1,index1) in items[active].children" :key="index1">
+          <!-- <div v-for="(l2,index2) in l1.children" :key="index2"> -->
+          <div class="l2-label">
+            <span>----{{l1.cat_name}}----</span>
+          </div>
+          <van-grid :border="true" :column-num="3" square clickable>
+            <van-grid-item v-for="(l3,index3) in l1.children" :key="index3">
+              <van-image :src="l3.cat_icon" show-loading />
+            </van-grid-item>
+          </van-grid>
+          <!-- </div> -->
+        </div>
+      </template>
+    </van-tree-select>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                active: 0,
-                items: []
-
-            };
-        },
-        created() {
-            this.initTreeSelect()
-        },
-        methods: {
-            changeData(index) {
-                console.log(index)
+export default {
+  data() {
+    return {
+      active: 0,
+      items: [
+        {
+          cat_deleted: false,
+          cat_icon: "",
+          cat_id: 1,
+          cat_level: 0,
+          cat_name: "",
+          text: "",
+          cat_pid: 0,
+          children: [
+            {
+              cat_deleted: false,
+              cat_icon: "",
+              cat_id: 1,
+              cat_level: 0,
+              cat_name: "",
+              text: "",
+              cat_pid: 0,
+              children: [
+                {
+                  cat_deleted: false,
+                  cat_icon: "",
+                  cat_id: 1,
+                  cat_level: 0,
+                  cat_name: "",
+                  text: "",
+                  cat_pid: 0,
+                },
+              ],
             },
-            initTreeSelect() {
-                const self = this;
-                const url = '/categories';
-                self.$http.get(url, {}).then(function (response) {
-                    //self.items = response.data.message;
-                    // for(var i = 0;i < self.items.length;i++){
-                    //     var obj = self.items[i];
-                    //     for(var key in obj){
-                    //         var newKey = self.keyMap[key];
-                    //         if(newKey){
-                    //             obj[newKey] = obj[key];
-                    //             delete obj[key];
-                    //         }
-                    //     }
-                    // }
-                    self.items = response.data.message.map(v => Object.assign(v, {text: v.cat_name}))
-                    console.log(self.items);
-                }).catch(function (error) {
-                    console.log(error);
-                })
-            }
-        }
-
+          ],
+        },
+      ],
     };
+  },
+  created() {
+    this.initTreeSelect();
+  },
+  methods: {
+    changeData(index) {
+      console.log(index);
+    },
+    initTreeSelect() {
+      const self = this;
+      const url = "/categories";
+      self.$http
+        .get(url, {})
+        .then(function (response) {
+          //self.items = response.data.message;
+          // for(var i = 0;i < self.items.length;i++){
+          //     var obj = self.items[i];
+          //     for(var key in obj){
+          //         var newKey = self.keyMap[key];
+          //         if(newKey){
+          //             obj[newKey] = obj[key];
+          //             delete obj[key];
+          //         }
+          //     }
+          // }
+          self.items = response.data.message.map((v) =>
+            Object.assign(v, { text: v.cat_name })
+          );
+          self.items.pop()
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
-<style scoped>
-
+<style>
 </style>
